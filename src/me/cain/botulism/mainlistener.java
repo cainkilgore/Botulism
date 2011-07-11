9cb62267e1e0ccb46e4e197e5ade71ca325815c9
CainFoool <cain.donaghey@live.co.uk>
2011-07-11 02:40:53 +0100

    fixes.

--- src/me/cain/botulism/mainlistener.java
+++ src/me/cain/botulism/mainlistener.java
@@ -14,11 +14,11 @@ import org.bukkit.inventory.ItemStack;
 public class mainlistener extends PlayerListener{
 	Player player;
 	Server server;
-	public void onPlayerInteract(PlayerInteractEvent event, Location CainFool) {
+	public void onPlayerInteract(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		if(event.getMaterial() == Material.PORK || event.getMaterial() == Material.SUGAR || event.getMaterial() == Material.RAW_FISH) {
 			if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-				player.damage(7);
+				player.damage(5);
 				player.sendMessage(ChatColor.RED + "Eating that is bad for you!");
 				event.getPlayer().setItemInHand(new ItemStack(0,0));
 			}
